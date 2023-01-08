021dba6cab1ea98b022d50a101e70510137429a6
Makkensh1 <61844918+Makkensh1@users.noreply.github.com>
2023-01-08 18:02:29 +0300

    config folder added

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,4 +1,4 @@
-package web;
+package web.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -1,4 +1,4 @@
-package web;
+package web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
